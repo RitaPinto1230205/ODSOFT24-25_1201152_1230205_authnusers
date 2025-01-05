de9b21146d196655ef4aaa52cccf69fe7b5a6e02
RitaPinto1230205 <1230205@isep.ipp.pt>
2025-01-05 20:13:02 +0000

    rollback

--- lms-authnusers/src/main/java/pt/psoft/g1/psoftg1/usermanagement/publishers/UserEventsPublisher.java
+++
@@ -1,9 +0,0 @@
-package pt.psoft.g1.psoftg1.usermanagement.publishers;
-
-import pt.psoft.g1.psoftg1.usermanagement.api.UserViewAMQP;
-import pt.psoft.g1.psoftg1.usermanagement.model.User;
-
-public interface UserEventsPublisher {
-    UserViewAMQP sendUserCreated(User user);
-    UserViewAMQP sendUserUpdated(User user, Long version);
-}
