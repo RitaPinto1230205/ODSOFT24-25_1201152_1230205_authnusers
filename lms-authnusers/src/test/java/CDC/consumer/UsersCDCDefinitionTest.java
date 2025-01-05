6681524c604251678b969d12986965fb1ea2469a
RitaPinto1230205 <1230205@isep.ipp.pt>
2025-01-05 02:28:07 +0000

    contract tests

--- lms-authnusers/src/test/java/CDC/consumer/UsersCDCDefinitionTest.java
+++ lms-authnusers/src/test/java/CDC/consumer/UsersCDCDefinitionTest.java
@@ -1,63 +1,64 @@
 package CDC.consumer;
 
-import au.com.dius.pact.consumer.MessagePactBuilder;
 import au.com.dius.pact.consumer.dsl.PactBuilder;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import au.com.dius.pact.core.model.messaging.MessagePact;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @ExtendWith(PactConsumerTestExt.class)
 @PactTestFor(providerName = "user_event-producer")
 public class UsersCDCDefinitionTest {
 
-    @Pact(consumer = "user_event-consumer")
-    public MessagePact userCreatedPact(MessagePactBuilder builder) {
+    @Pact(provider = "user_event-producer", consumer = "user_event-consumer")
+    public V4Pact userCreatedPact(PactBuilder builder) throws Exception {
+        Map<String, Object> message = Map.of(
+                "username", "testUser",
+                "name", "Test User"
+        );
+
         return builder
-                .given("a user created event")
-                .expectsToReceive("user created message")
-                .withContent("{" +
-                        "\"username\": \"testUser\"," +
-                        "\"name\": \"Test User\"" +
-                        "}")
+                .given("a user created event exists")
+                .hasPactWith("user_event-producer")
+                .synchronousMessage("a user created event")
+                .withContent(message)
                 .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "userCreatedPact")
-    void testUserCreatedEvent(MessagePact pact) {
-        // Obtém a mensagem do contrato
-        String message = new String(pact.getMessages().get(0).contentsAsBytes());
-
-        // Verifica se a mensagem contém os campos esperados
+    void testUserCreatedEvent(V4Pact pact) {
+        var message = pact.getInteractions().get(0).asMessage().contentsAsString();
         assertTrue(message.contains("testUser"));
         assertTrue(message.contains("Test User"));
     }
 
-    @Pact(consumer = "user_event-consumer")
-    public MessagePact userUpdatedPact(MessagePactBuilder builder) {
+    @Pact(provider = "user_event-producer", consumer = "user_event-consumer")
+    public V4Pact userUpdatedPact(PactBuilder builder) throws Exception {
+        Map<String, Object> message = Map.of(
+                "username", "testUser",
+                "name", "Updated User"
+        );
+
         return builder
-                .given("a user updated event")
-                .expectsToReceive("user updated message")
-                .withContent("{" +
-                        "\"username\": \"testUser\"," +
-                        "\"name\": \"Test User\"" +
-                        "}")
+                .given("a user updated event exists")
+                .hasPactWith("user_event-producer")
+                .synchronousMessage("a user updated event")
+                .withContent(message)
                 .toPact();
     }
 
     @Test
     @PactTestFor(pactMethod = "userUpdatedPact")
-    void testUserUpdatedEvent(MessagePact pact) {
-        // Obtém a mensagem do contrato
-        String message = new String(pact.getMessages().get(0).contentsAsBytes());
-
-        // Verifica se a mensagem contém os campos esperados
+    void testUserUpdatedEvent(V4Pact pact) {
+        var message = pact.getInteractions().get(0).asMessage().contentsAsString();
         assertTrue(message.contains("testUser"));
         assertTrue(message.contains("Updated User"));
     }
